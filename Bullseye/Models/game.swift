ef89d90babc6fa867063e2fc771f44293a24ff28
Elyse Roth <elysemorganroth@gmail.com>
2022-02-07 09:26:37 -0500

    first part of ray w ios practice

---
+++ Bullseye/Models/game.swift
@@ -0,0 +1,19 @@
+//
+//  game.swift
+//  Bullseye
+//
+//  Created by lightyear on 2/3/22.
+//
+
+import Foundation
+
+struct Game {
+    var target = Int.random(in: 1...100)
+    var score = 0
+    var round = 1
+    
+    func points(sliderValue: Int) -> Int {
+       100 - abs(self.target - sliderValue)
+    }
+    
+}
