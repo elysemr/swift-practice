8ceedea6bac08c911f3659533f2d43800980d076
Elyse Roth <elysemorganroth@gmail.com>
2022-02-15 14:59:21 -0500

    added headers to the leaderboard view

--- Bullseye/Views/LeaderboardView.swift
+++ Bullseye/Views/LeaderboardView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct LeaderboardView: View {
     var body: some View {
-        RowView(index: 1, score: 10, date: Date())
+        ZStack {
+            Color("BackgroundColor")
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 10) {
+                HeaderView()
+                LabelView()
+            RowView(index: 1, score: 10, date: Date())
+            }
+        }
     }
 }
 
@@ -37,6 +45,40 @@ struct RowView: View {
         .padding(.leading)
         .padding(.trailing)
         .frame(maxWidth: Constants.Leaderboard.leaderboardMaxRowWidth)
+    }
+}
+
+struct HeaderView: View {
+    var body: some View {
+        ZStack{
+        BigBoldText(text: "Leaderboard")
+            HStack {
+                Spacer()
+                Button(action: {}) {
+                RoundedImageViewFilled(systemName: "xmark")
+                    .padding(.trailing)
+                }
+            }
+        }
+    }
+}
+
+struct LabelView: View {
+    
+    var body: some View {
+        HStack{
+            Spacer()
+                .frame(width: Constants.General.roundedViewLength)
+            Spacer()
+            LabelText(text: "Score")
+                .frame(width: Constants.Leaderboard.leaderboardScoreColWidth)
+            Spacer()
+            LabelText(text: "Date")
+                .frame(width: Constants.Leaderboard.leaderboardDateColWidth)
+        }
+            .padding(.leading)
+            .padding(.trailing)
+            .frame(maxWidth: Constants.Leaderboard.leaderboardMaxRowWidth)
     }
 }
 
